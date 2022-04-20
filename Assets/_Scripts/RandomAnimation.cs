cf4883287afdf9ab87d78b03199dc451625ff0b2
troyle <tj_royle@hotmail.co.uk>
2022-04-21 00:33:48 +0100

    added ability to play random animations with a left click
    make sure that the animations are loaded into the animator so it can be picked up by the code

---
+++ Assets/_Scripts/RandomAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimation : MonoBehaviour
+{
+    private Animator animator;
+    public List<string> animations = new List<string>();
+
+    void Awake()
+    {
+        animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+
+        //Load animations
+        animations.Clear();
+        foreach(AnimationClip ac in animator.runtimeAnimatorController.animationClips)
+            animations.Add(ac.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Left Click
+        {
+            string anim = animations[Random.Range(0, animations.Count)];
+            animator.Play(anim);
+            Debug.Log($"Playing {anim} animation");
+        }
+    }
+}
