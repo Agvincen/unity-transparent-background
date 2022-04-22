f5405dd41bd2dc9d96121eb73ec9799fb9846b82
troyle <tj_royle@hotmail.co.uk>
2022-04-22 18:00:08 +0100

    made the idle animation in the Faith model loop
    added once random anim is finished, idle plays again
    
    mouse disapears if you start to rotate the model and does not stop at the end of the monitor
    
    flick the model and let go for it to have a bit of a spin

--- Assets/_Scripts/MouseRotate.cs
+++ Assets/_Scripts/MouseRotate.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
+[System.Serializable]
+public struct POINT
+{
+    public int X;
+    public int Y;
+}
 public class MouseRotate : MonoBehaviour
 {
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int X, int Y);
+    [DllImport("user32.dll")]
+    static extern bool GetCursorPos(out POINT lpPoint);
+
     public GameObject Model;
-    public Vector2 RoatationSensitivity = Vector2.right;
+    public Vector2 RotationSensitivity = Vector2.right;
+    public Vector2 RotationDamping = Vector2.right;
 
     private Collider _collider;
     private Camera _camera;
     private bool _rotationEnabled;
-    private Vector2 _prevMouse;
+    private bool _refresh;
+    private Vector2 _releaseVelocity;
     private Vector2 _mouse;
+    public POINT _mousePos;
 
     void Awake()
     {
@@ -26,24 +41,52 @@ public class MouseRotate : MonoBehaviour
 
     void Update()
     {
+        if (_refresh)
+        {
+            SetCursorPos(_mousePos.X, _mousePos.Y);
+            Cursor.visible = true;
+            _refresh = false;
+        }
+
         _mouse = Input.mousePosition;
 
-        if(Input.GetMouseButtonDown(0)) //Left Click
+        if (Input.GetMouseButtonDown(0)) //Left Click
         {
             Ray ray = _camera.ScreenPointToRay(_mouse);
-            if(Physics.Raycast(ray, out _))
+            if (Physics.Raycast(ray, out _))
                 _rotationEnabled = true;
         }
         else if (Input.GetMouseButtonUp(0))
         {
             _rotationEnabled = false;
+
+            Cursor.lockState = CursorLockMode.None;
+            _refresh = true;
         }
         else if (_rotationEnabled)
         {
-            Vector2 spin = (_mouse - _prevMouse) * RoatationSensitivity;
-            Model.transform.Rotate(new Vector2(spin.y, -spin.x) * Time.deltaTime);
-        }
+            Vector2 spin = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * RotationSensitivity;
+            if(Cursor.visible && spin.magnitude > 0.01)
+            {
+                GetCursorPos(out _mousePos);
 
-        _prevMouse = _mouse;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            _releaseVelocity = spin;
+            RotateModel(spin);
+        }
+        else
+        {
+            if(_releaseVelocity.magnitude > 0.1f)
+            {
+                _releaseVelocity *= RotationDamping;
+                RotateModel(_releaseVelocity);
+            }
+        }
+    }
+    void RotateModel(Vector2 rot)
+    {
+        Model.transform.Rotate(new Vector2(rot.y, -rot.x) * Time.deltaTime);
     }
 }
--- Assets/_Scripts/RandomAnimation.cs
+++ Assets/_Scripts/RandomAnimation.cs
@@ -6,6 +6,7 @@ public class RandomAnimation : MonoBehaviour
 {
     private Animator animator;
     public List<string> animations = new List<string>();
+    public float prevAnimTime = 0f;
 
     void Awake()
     {
@@ -22,9 +23,23 @@ public class RandomAnimation : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) //Left Click
         {
-            string anim = animations[Random.Range(0, animations.Count)];
-            animator.Play(anim);
-            Debug.Log($"Playing {anim} animation");
+            PlayAnim(Random.Range(1, animations.Count));
+            prevAnimTime = 0;
         }
+        else
+        {
+            float animLoop = Mathf.Repeat(animator.GetCurrentAnimatorStateInfo(0).normalizedTime, 1f);
+            if (animLoop < prevAnimTime)
+            {
+                PlayAnim(0);
+            }
+            prevAnimTime = animLoop;
+        }
+    }
+    void PlayAnim(int index)
+    {
+        string anim = animations[index];
+        animator.Play(anim);
+        Debug.Log($"Playing {anim} animation");
     }
 }
